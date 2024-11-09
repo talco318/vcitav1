2c6ec5adcdc4796ceed122ca08d77dfd3e61cad4
Tal Cohen <talcohen318@gmail.com>
2024-11-09 21:42:32 +0200

    more user friendly main screen

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link for routing
 
-const client_uri = 'https://vcita-playground.web.app/oauth&redirect_uri=https://a6ae-85-250-29-181.ngrok-free.app';
+// src/components/Home.js
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Home = ({ clientId }) => {
-    console.log('Client ID:', clientId);
+    const location = useLocation();
+    const error = location.state?.error;
+
+    const authUrl = `https://app.vcita.com/app/oauth/authorize?client_id=${clientId}&redirect_uri=https://vcita-playground.web.app/oauth&state=code`;
 
     return (
         <div className="home-container">
-            <h1>Hello, you have to authorize first</h1>
+            <h1 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                Welcome to Client Manager
+            </h1>
+            <h2>You have to authorize first</h2>
+            {error && (
+                <div className="mt-4 p-4 bg-red-100 text-red-700 rounded-lg">
+                    {error}
+                </div>
+            )}
+            <div>
             <Link
-                to={`https://app.vcita.com/app/oauth/authorize?client_id=${clientId}&redirect_uri=${client_uri}`}
+                to={`https://app.vcita.com/app/oauth/authorize?client_id=${clientId}&redirect_uri=${authUrl}`}
                 className="authorize-button"
             >
                 Authorize
             </Link>
+            </div>
         </div>
     );
 };
 
 export default Home;
+
+
